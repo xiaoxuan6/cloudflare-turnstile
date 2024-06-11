63180c835d6c59bf2acd6414e3ead5f25b5c8488
xiaoxuan6 <1527736751@qq.com>
2024-06-11 11:04:00 +0800

    feat: mod init

--- api/api.go
+++ api/api.go
@@ -1,16 +1,16 @@
 package api
 
 import (
-	"fmt"
+	"github.com/sirupsen/logrus"
 	"net/http"
 )
 
 func Api(w http.ResponseWriter, r *http.Request) {
 	_ = r.ParseForm()
 
-	fmt.Printf("email: %s", r.PostForm.Get("email"))
-	fmt.Printf("password: %s", r.PostForm.Get("password"))
-	fmt.Printf("cf-turnstile-response: %s", r.PostForm.Get("cf-turnstile-response"))
+	logrus.Info("email: %s", r.PostForm.Get("email"))
+	logrus.Info("password: %s", r.PostForm.Get("password"))
+	logrus.Info("cf-turnstile-response: %s", r.PostForm.Get("cf-turnstile-response"))
 
 	_, _ = w.Write([]byte("ok"))
 }
